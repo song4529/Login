a7f3830affb57162758793143c0dfdc4b4482d8b
song4529 <gusdn4529@gmail.com>
2024-02-04 18:18:26 +0900

    feat : User 기본 필드 작성

--- src/main/java/com/example/login/user/entity/User.java
+++ src/main/java/com/example/login/user/entity/User.java
@@ -1,4 +1,18 @@
 package com.example.login.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
 }
