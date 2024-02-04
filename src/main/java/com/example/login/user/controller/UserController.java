4e974192479caa1600e3074612536eb860739f4d
song4529 <gusdn4529@gmail.com>
2024-02-04 18:18:56 +0900

    feat : 애너테이션 추가

--- src/main/java/com/example/login/user/controller/UserController.java
+++ src/main/java/com/example/login/user/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.example.login.user.controller;
 
+import com.example.login.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
 public class UserController {
 }
--- src/main/java/com/example/login/user/service/UserService.java
+++ src/main/java/com/example/login/user/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.login.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
