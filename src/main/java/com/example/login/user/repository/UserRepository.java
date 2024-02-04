c8916d773f82b0fe5ddc953ca4e8a0f356078702
song4529 <gusdn4529@gmail.com>
2024-02-04 18:18:40 +0900

    feat : 의존성 추가

--- src/main/java/com/example/login/user/repository/UserRepository.java
+++ src/main/java/com/example/login/user/repository/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.login.user.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
 }
