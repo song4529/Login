121b32c2ff7ede977dcacb789cbb3345a1581c04
song4529 <gusdn4529@gmail.com>
2024-02-05 23:44:25 +0900

    feat : 의존관계 수정 및 findByUserId 메서드 추가

--- src/main/java/com/example/login/user/repository/UserRepository.java
+++ src/main/java/com/example/login/user/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.login.user.repository;
 
+import com.example.login.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(long userId);
 }
