a18448d5b9a1718f0e524d8c2a01d5aa49546ee8
song4529 <gusdn4529@gmail.com>
2024-02-05 23:44:00 +0900

    feat : getter setter 애너테이션 추가

--- src/main/java/com/example/login/user/entity/User.java
+++ src/main/java/com/example/login/user/entity/User.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +19,7 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String password;
 }
